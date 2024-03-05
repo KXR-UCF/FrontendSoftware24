44c61550b03c185bb61790945b1c933f13f1b36a
Christopher <christopher.blessing@icloud.com>
2024-03-05 12:33:08 -0500

    Started implementing modules

--- ConfigGUI/KXR_LTI_config-settings.py
+++ ConfigGUI/KXR_LTI_config-settings.py
@@ -5,6 +5,7 @@ from tkinter import messagebox
 from tkinter import colorchooser
 import os
 import socket
+from tv_communication import *
 
 
 
@@ -198,23 +199,10 @@ def stm32_send():
     # may not need these
     stm32_log.write(tv_entry_mode.get()+'\n')
     stm32_log.write(tv_entry_rocket.get()+'\n')
-    
+
     stm32_log.close()
-
-# sending tv data
-def tv_send():
-    print("mode: " + tv_entry_mode.get())
-    print("rocket: " + tv_entry_rocket.get())
     
-    # loop through adc channels
-    for channel in wanda_adc_channels.adc_channels:
-        if channel.entry_channel.current() != 0:
-            print("channel: " + str(channel.entry_channel.current()-1))
-            print("type: " + str(channel.entry_type.current()))
-            print("serial: " + str(channel.entry_serial.get()))
-            print("color: " + str(channel.color_code))
-
-# Wanda communication
+    # Wanda communication
 # send unsigned, 32-bit int over network
 def send_int(number: int):
     wandaSocket.sendall(number.to_bytes(length=4, byteorder="little", signed=False))
---
+++ ConfigGUI/tv_communication.py
@@ -0,0 +1,14 @@
+import KXR_LTI_config-settings
+
+# sending tv data
+def tv_send():
+    print("mode: " + tv_entry_mode.get())
+    print("rocket: " + tv_entry_rocket.get())
+    
+    # loop through adc channels
+    for channel in wanda_adc_channels.adc_channels:
+        if channel.entry_channel.current() != 0:
+            print("channel: " + str(channel.entry_channel.current()-1))
+            print("type: " + str(channel.entry_type.current()))
+            print("serial: " + str(channel.entry_serial.get()))
+            print("color: " + str(channel.color_code))
